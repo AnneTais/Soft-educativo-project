e39b7b942be8b1d91a00e6fa81774c8c7eaf8427
Kaue Bezerra <149630716+KaueBezerra11@users.noreply.github.com>
2025-05-20 23:37:41 -0300

    Delete src directory

--- src/App.test.js
+++
@@ -1,9 +0,0 @@
-import { render, screen } from '@testing-library/react';
-// import HomePage from './Pages/Home/HomePage';
-// import LoginPage from './Pages/Login/LoginPage';
-import PerfilPage from './Pages/Perfil/PerfilPage';
-test('renders learn react link', () => {
-  render(<PerfilPage />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
